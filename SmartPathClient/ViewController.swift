b5e350c1a6bcaa433df4d358b9f95aeefa199753
Glenn Schmidt <glenn@codeacious.com>
2016-07-30 12:14:58 +1000

    Add user tracking button to map

--- SmartPathClient/ViewController.swift
+++ SmartPathClient/ViewController.swift
@@ -10,12 +10,26 @@ import UIKit
 import GoogleMaps
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet private weak var mapContainer: UIView!
+    @IBOutlet private weak var locateMeButton: UIButton!
     
     private var mapView: GMSMapView!
     
     private var isVisible: Bool = false
     private var hasLocation: Bool = false
+    
+    private var userTrackingEnabled: Bool = false {
+        didSet {
+            self.locateMeButton.selected = userTrackingEnabled
+            if userTrackingEnabled {
+                self.locationManager.startUpdatingLocation()
+            } else {
+                self.locationManager.stopUpdatingLocation()
+            }
+        }
+    }
     
     private lazy var locationManager = CLLocationManager()
     
@@ -25,10 +39,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.cameraWithLatitude(-33.86, longitude: 151.20, zoom: 6.0)
+        
+        // Create the map view
         self.mapView = GMSMapView.mapWithFrame(CGRect.zero, camera: camera)
+        self.mapView.settings.rotateGestures = false
+        self.mapView.settings.tiltGestures = false
+        self.mapView.delegate = self
         self.mapView.autoresizingMask = [.FlexibleWidth, .FlexibleHeight]
-        self.mapView.frame = self.view.bounds
-        self.view.addSubview(mapView)
+        self.mapView.frame = self.mapContainer.bounds
+        self.mapContainer.addSubview(mapView)
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
@@ -42,8 +61,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         super.viewDidAppear(animated)
         self.isVisible = true
         
+        self.locationManager.delegate = self
         if (CLLocationManager.authorizationStatus() == .NotDetermined) {
-            self.locationManager.delegate = self
             self.locationManager.requestWhenInUseAuthorization()
         } else {
             self.mapView.myLocationEnabled = true
@@ -56,11 +75,37 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     
+    //MARK: GMSMapViewDelegate
+    
+    func mapView(mapView: GMSMapView, willMove gesture: Bool) {
+        if gesture {
+            self.userTrackingEnabled = false
+        }
+    }
+    
+    
     //MARK: CLLocationManagerDelegate
     
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         if (status == .AuthorizedAlways || status == .AuthorizedWhenInUse) {
             self.mapView.myLocationEnabled = true
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if self.userTrackingEnabled {
+            self.mapView.animateToLocation(locations[0].coordinate)
+        }
+    }
+    
+    
+    //MARK: Actions
+    
+    @IBAction func toggleUserTracking(sender: AnyObject) {
+        if self.userTrackingEnabled {
+            self.userTrackingEnabled = false
+        } else if (!self.handleLocationDenied()) {
+            self.userTrackingEnabled = true
         }
     }
     
