8de05c2e3657bfad4cfd7b11c8e6e9865495818c
Rodger Benham <rodgerbenham@Rodgers-MacBook-Pro.local>
2016-07-30 10:59:33 +1000

    Google maps is now shown on ViewController

--- SmartPathClient/AppDelegate.swift
+++ SmartPathClient/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAM2PI58TjKpsyu038eW8wdbC-StauLw7g")
+        
         return true
     }
 
